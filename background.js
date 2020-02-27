0f2c95399eb0eb0fca30d351998f420fdc04854a
Markus Waitl <markus.waitl@ecosia.org>
2020-02-27 15:39:51 +0100

    use badge color to check state

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
 /* Define EditableContentBg Module */
 const browserAPI = browser || chrome;
 
+const CSS = "body { border: 10px solid red; transition: border; }";
+
 const EditableContentBg = (() => {
     let init,
         toggleEditable;
-
-    const activatedInTabs = [];
 
     /**
      * toggleEditable
@@ -15,20 +15,37 @@ const EditableContentBg = (() => {
      *
      * @param {Object} tab - current tab object
      **/
-    toggleEditable = function(tab) {
-        const tabIndex = activatedInTabs.indexOf(tab.id);
-        if (tabIndex >= 0) {
-             browserAPI.browserAction.setIcon({tabId: tab.id, path: "icon.png"});
-            browserAPI.tabs.executeScript({
+    toggleEditable = async (tab) => {
+        const badgeColor = await browserAPI.browserAction.getBadgeBackgroundColor({
+            tabId: tab.id,
+        });
+
+        if (badgeColor && badgeColor[0] === 0) {
+            browserAPI.browserAction.setIcon({
+                tabId: tab.id,
+                path: 'icon.png'
+            });
+            browserAPI.browserAction.setBadgeBackgroundColor({
+                tabId: tab.id,
+                color: 'white',
+            });
+            browserAPI.tabs.executeScript(tab.id, {
                 code: 'document.body.removeAttribute("contenteditable");'
             });
-            activatedInTabs.splice(tabIndex, 1);
+            browser.tabs.removeCSS(tab.id, { code: CSS });
         } else {
-             browserAPI.browserAction.setIcon({tabId: tab.id, path: "icon-active.png"});
-             browserAPI.tabs.executeScript({
+            browserAPI.browserAction.setIcon({
+                tabId: tab.id,
+                path: 'icon-active.png'
+            });
+            browserAPI.tabs.executeScript(tab.id,{
                 code: 'document.body.setAttribute("contenteditable", true);'
             });
-            activatedInTabs.push(tabIndex);
+            browserAPI.browserAction.setBadgeBackgroundColor({
+                tabId: tab.id,
+                color: 'black',
+            });
+            browser.tabs.insertCSS(tab.id, { code: CSS });
         }
     };
 
@@ -37,7 +54,7 @@ const EditableContentBg = (() => {
      *
      * Sets browserAction and tabs events
      **/
-    init = function() {
+    init = () => {
         browserAPI.browserAction.onClicked.addListener(toggleEditable);
     };
 
