ca8913b8b7103da8c5f4012fcd3bd9cd25e6437f
Markus Waitl <markus.waitl@ecosia.org>
2022-01-27 13:57:03 +0100

    Migrate to manifest 3

--- background.js
+++ background.js
@@ -1,66 +1,45 @@
-/* Define EditableContentBg Module */
-const browserAPI = browser || chrome;
+const browserAPI = chrome;
 
-const CSS = "body { border: 10px solid red; transition: border; }";
+const css = "body { border: 10px solid red; transition: border; }";
 
-const EditableContentBg = (() => {
-    let init,
-        toggleEditable;
+const enableContentEditable = () => {
+    document.body.setAttribute("contenteditable", true);
+};
 
-    /**
-     * toggleEditable
-     *
-     * Toggles contenteditable value on the current page
-     * by injecting content.js
-     *
-     * @param {Object} tab - current tab object
-     **/
-    toggleEditable = async (tab) => {
-        const badgeColor = await browserAPI.browserAction.getBadgeBackgroundColor({
+const disableContentEditable = () => {
+    document.body.removeAttribute("contenteditable");
+};
+
+const toggleEditable = async (tab) => {
+    const tabId = tab.id;
+
+    const badgeColor = await browserAPI.action.getBadgeBackgroundColor({
+        tabId,
+    });
+
+    if (badgeColor && badgeColor[1] === 128) {
+        browserAPI.action.setIcon({
             tabId: tab.id,
+            path: 'icon.png'
         });
+        chrome.action.setBadgeBackgroundColor({
+            tabId: tab.id,
+            color: 'red',
+        });
+        chrome.scripting.executeScript({ target: { tabId }, function: disableContentEditable });
+        browserAPI.scripting.removeCSS({ target: { tabId }, css });
+    } else {
+        browserAPI.action.setIcon({
+            tabId: tab.id,
+            path: 'icon-active.png'
+        });
+        chrome.scripting.executeScript({ target: { tabId }, function: enableContentEditable });
+        browserAPI.action.setBadgeBackgroundColor({
+            tabId: tab.id,
+            color: 'green',
+        });
+        chrome.scripting.insertCSS({ target: { tabId: tab.id }, css });
+    }
+};
 
-        if (badgeColor && badgeColor[0] === 0) {
-            browserAPI.browserAction.setIcon({
-                tabId: tab.id,
-                path: 'icon.png'
-            });
-            browserAPI.browserAction.setBadgeBackgroundColor({
-                tabId: tab.id,
-                color: 'white',
-            });
-            browserAPI.tabs.executeScript(tab.id, {
-                code: 'document.body.removeAttribute("contenteditable");'
-            });
-            browser.tabs.removeCSS(tab.id, { code: CSS });
-        } else {
-            browserAPI.browserAction.setIcon({
-                tabId: tab.id,
-                path: 'icon-active.png'
-            });
-            browserAPI.tabs.executeScript(tab.id,{
-                code: 'document.body.setAttribute("contenteditable", true);'
-            });
-            browserAPI.browserAction.setBadgeBackgroundColor({
-                tabId: tab.id,
-                color: 'black',
-            });
-            browser.tabs.insertCSS(tab.id, { code: CSS });
-        }
-    };
-
-    /**
-     * init
-     *
-     * Sets browserAction and tabs events
-     **/
-    init = () => {
-        browserAPI.browserAction.onClicked.addListener(toggleEditable);
-    };
-
-    return {
-        init: init
-    };
-})();
-
-EditableContentBg.init();
+chrome.action.onClicked.addListener(toggleEditable);
